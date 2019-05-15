878be6adc4518931a8f3a01799beab13d6373bca
João Pedro Mantovani <jpm.mantovani45@gmail.com>
2019-05-15 19:15:15 -0300

    Header done from form page
    Took 1 hour 10 minutes

--- src/app/modules/user/pages/user-form-page/user-form-page.component.ts
+++ src/app/modules/user/pages/user-form-page/user-form-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../../../../../shared/models/user.model';
 import {BreadcrumbService} from '../../../../../shared/components/breadcrumb/breadcrumb.service';
+import {faPuzzlePiece, faTrophy, faSign, faTrash} from '@fortawesome/free-solid-svg-icons/';
+import {library} from '@fortawesome/fontawesome-svg-core';
+
+library.add(faPuzzlePiece, faTrophy, faSign);
 
 @Component({
   selector: 'app-user-form-page',
@@ -10,8 +14,17 @@ import {BreadcrumbService} from '../../../../../shared/components/breadcrumb/bre
 export class UserFormPageComponent implements OnInit {
 
   newUser: User;
+  faPuzzlePiece = faPuzzlePiece;
+  faTrophy = faTrophy;
+  faSign = faSign;
 
   constructor(private breadcrumb: BreadcrumbService) { }
+
+  icons: any[] = [
+    {icon: faPuzzlePiece, h5: 'Cycling', small: 'Sport Type'},
+    {icon: faTrophy, h5: 'Advanced', small: 'Mode'},
+    {icon: faSign, h5: '30 miles', small: 'Route'},
+  ];
 
   ngOnInit() {
     setTimeout(() =>
