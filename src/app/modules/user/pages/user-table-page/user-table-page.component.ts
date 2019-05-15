e554708c7f1b43921628eaf0f305df09df29bf64
João Pedro Mantovani <jpm.mantovani45@gmail.com>
2019-05-15 19:58:32 -0300

    Bugfix header form page
    Took 3 minutes

--- src/app/modules/user/pages/user-table-page/user-table-page.component.ts
+++ src/app/modules/user/pages/user-table-page/user-table-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {BreadcrumbService} from '../../../../../shared/components/breadcrumb/breadcrumb.service';
 import {Title} from '@angular/platform-browser';
+import {faPuzzlePiece, faSign, faTrophy} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-user-table-page',
@@ -9,6 +10,16 @@ import {Title} from '@angular/platform-browser';
 })
 export class UserTablePageComponent implements OnInit {
   @Input() newUser: any;
+
+  faPuzzlePiece = faPuzzlePiece;
+  faTrophy = faTrophy;
+  faSign = faSign;
+
+  icons: any[] = [
+    {icon: faPuzzlePiece, h5: 'Cycling', small: 'Sport Type'},
+    {icon: faTrophy, h5: 'Advanced', small: 'Mode'},
+    {icon: faSign, h5: '30 miles', small: 'Route'},
+  ];
 
   constructor(private breadcrumb: BreadcrumbService,
               private titleService: Title) { }
