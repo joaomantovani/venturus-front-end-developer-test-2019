80d8cdd932fa8e96b90a656b738029a569cb5479
João Pedro Mantovani <jpm.mantovani45@gmail.com>
2019-05-15 23:05:01 -0300

    404 bugfix
    Took 6 minutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'users'},
   {path: 'users', loadChildren: './modules/user/user.module#UserModule'},
+  {path: '**', redirectTo: 'users'},
 ];
 
 @NgModule({
